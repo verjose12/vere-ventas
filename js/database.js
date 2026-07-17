d22171bb69b4789c977d2a0fd6c041f0f4a64f47
verjose12 <al22220113@utcj.edu.mx>
2026-07-16 21:16:46 -0600

    pruebas de BD - projectURL

--- js/database.js
+++ js/database.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://hmdxqtcxsafqoympyttr.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://hmdxqtcxsafqoympyttr.supabase.co";
 const SUPABASE_KEY = "sb_publishable_l6yOXCyNUW9FSNOd7h6f5g_SeSI7aJD";
 
 const supabaseClient = supabase.createClient(
